e3e0708c1f2050b3d044089ed0aa63c358e8a81f
maher <maher.ahmed@engaz.ai>
2025-11-19 15:47:45 +0200

    create forgatpass component

---
+++ apps/Fitness/src/app/features/layouts/auth/components/forgetpass/components/confirm-otp/confirm-otp.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-confirm-otp',
+  imports: [],
+  templateUrl: './confirm-otp.html',
+  styleUrl: './confirm-otp.scss',
+})
+export class ConfirmOtp {
+
+}
---
+++ apps/Fitness/src/app/features/layouts/auth/components/forgetpass/components/create-new-pass/create-new-pass.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-create-new-pass',
+  imports: [],
+  templateUrl: './create-new-pass.html',
+  styleUrl: './create-new-pass.scss',
+})
+export class CreateNewPass {
+
+}
