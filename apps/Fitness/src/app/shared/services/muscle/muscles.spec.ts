01389e5ee7d217442c45985132eaf38b85dd368a
mahmouddessoki <mahmouddessoki33@gmail.com>
2025-12-04 23:51:34 +0200

    meals and services testing

--- apps/Fitness/src/app/shared/services/muscle/muscles.spec.ts
+++ apps/Fitness/src/app/shared/services/muscle/muscles.spec.ts
@@ -1,16 +1,100 @@
-import { TestBed } from '@angular/core/testing';
+import {HttpTestingController, provideHttpClientTesting} from "@angular/common/http/testing";
+import {Muscles} from "./muscles";
+import {TestBed} from "@angular/core/testing";
+import {provideZonelessChangeDetection} from "@angular/core";
+import {muscleGroupRes, musclesRes} from "../../models/muscles";
+import {environment} from "@fitness-app/environment/baseUrl.dev";
 
-import { Muscles } from './muscles';
+describe("muscles service testing suite", () => {
+    //
+    let _muscleService: Muscles;
+    let httpTestingController: HttpTestingController;
 
-describe('Muscles', () => {
-  let service: Muscles;
+    beforeEach(() => {
+        TestBed.configureTestingModule({
+            providers: [Muscles, provideHttpClientTesting(), provideZonelessChangeDetection()],
+        });
 
-  beforeEach(() => {
-    TestBed.configureTestingModule({});
-    service = TestBed.inject(Muscles);
-  });
+        _muscleService = TestBed.inject(Muscles);
+        httpTestingController = TestBed.inject(HttpTestingController);
+    });
 
-  it('should be created', () => {
-    expect(service).toBeTruthy();
-  });
+    it("should be created", () => {
+        expect(_muscleService).toBeTruthy();
+    });
+
+    it("get all muscle groups test case", () => {
+        let resp: musclesRes | undefined;
+        const respMock = {
+            message: "success",
+            musclesGroup: [
+                {
+                    _id: "67c79f3526895f87ce0aa970",
+                    name: "Adductors",
+                },
+            ],
+        };
+        _muscleService.getAllMuscleGroups().subscribe((res) => {
+            resp = res;
+        });
+
+        const req = httpTestingController.expectOne(`${environment.baseApiUrl}muscles`);
+        req.flush(respMock);
+
+        expect(resp?.message).toBe("success");
+        expect(req.request.method).toBe("GET");
+    });
+    it("get all muscle by group test case", () => {
+        let resp: muscleGroupRes | undefined;
+        const id = "67c79f3526895f87ce0aa971";
+        const respMock = {
+            message: "success",
+            muscleGroup: {
+                _id: "67c79f3526895f87ce0aa971",
+                name: "Biceps",
+            },
+            muscles: [
+                {
+                    _id: "67c8499726895f87ce0aa9c2",
+                    name: "Biceps Brachii",
+                    image: "https://iili.io/33p7v6b.png",
+                },
+            ],
+        };
+        _muscleService.getAllMusclesByMuscleGroup(id).subscribe((res) => {
+            resp = res;
+        });
+
+        const req = httpTestingController.expectOne(`${environment.baseApiUrl}muscles`);
+        req.flush(respMock);
+
+        expect(resp).toEqual(respMock);
+        expect(req.request.method).toBe("GET");
+    });
+    it("get random muscles test case", () => {
+        let resp: muscleGroupRes | undefined;
+        const respMock = {
+            message: "success",
+            muscleGroup: {
+                _id: "67c79f3526895f87ce0aa971",
+                name: "Biceps",
+            },
+            muscles: [
+                {
+                    _id: "67c8499726895f87ce0aa9c2",
+                    name: "Biceps Brachii",
+                    image: "https://iili.io/33p7v6b.png",
+                },
+            ],
+        };
+        _muscleService.getRandomMuscles().subscribe((res) => {
+            resp = res;
+        });
+
+        const req = httpTestingController.expectOne(`${environment.baseApiUrl}muscles/random`);
+        req.flush(respMock);
+
+        expect(resp).toEqual(respMock);
+        expect(req.request.method).toBe("GET");
+    });
 });
