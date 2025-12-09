e37c17cfa3af0764af3d5036c6850661f863329a
Reham-Gomaa <rehamgomaa686@gmail.com>
2025-12-09 15:47:09 +0200

    feat: 🎸 add workout title and navtabs to details

--- apps/Fitness/src/app/shared/components/ui/details/details.ts
+++ apps/Fitness/src/app/shared/components/ui/details/details.ts
@@ -1,7 +1,13 @@
-import {Component, inject, OnInit, signal, WritableSignal} from "@angular/core";
-import {ActivatedRoute} from "@angular/router";
+import {Component, DestroyRef, effect, inject, OnInit, signal, WritableSignal} from "@angular/core";
+import {ActivatedRoute, Router} from "@angular/router";
 import {Panel} from "../business/panel/panel";
 import {NavTabs} from "../navTabs/navTabs";
+import {Muscles} from "../../../services/muscle/muscles";
+import {takeUntilDestroyed} from "@angular/core/rxjs-interop";
+import {MuscleGroup} from "../../../models/muscles";
+import {Exercise} from "../../../models/exercises";
+import {Meal} from "../../../models/meals";
+import {MealService} from "../../../services/meals/meals";
 
 @Component({
     selector: "app-details",
@@ -11,11 +17,32 @@ import {NavTabs} from "../navTabs/navTabs";
 })
 export class Details implements OnInit {
     private activatedRoute = inject(ActivatedRoute);
+    private _router = inject(Router);
+    private _muscleService = inject(Muscles);
+    private _mealService = inject(MealService);
+    private _destroyRef = inject(DestroyRef);
 
     id: WritableSignal<string> = signal<string>("");
-
     //to decide which api to call
     cat: WritableSignal<string> = signal<string>("");
+    workout_muscles = signal<MuscleGroup[]>([]);
+
+    selectedExercise = signal<Exercise | null>(null);
+    selectedMeal = signal<Meal | null>(null);
+
+    private levelsEffect = effect(() => {
+        this.getSelectedExercise();
+        this.getSelectedMeal();
+    });
+
+    getSelectedExercise() {
+        this.selectedExercise.set(this._muscleService.selectedExercise());
+        console.log(this.selectedExercise());
+    }
+    getSelectedMeal() {
+        this.selectedMeal.set(this._mealService.getSelectedMeal());
+        console.log(this.selectedMeal());
+    }
 
     ngOnInit(): void {
         this.getItemId();
@@ -25,6 +52,32 @@ export class Details implements OnInit {
         this.activatedRoute.paramMap.subscribe((res) => {
             this.id.set(res.get("id") as string);
             this.cat.set(res.get("cat") as string);
+            if (this._router.url.includes("classes")) {
+                this.getAllMuscles();
+            }
         });
+    }
+
+    getAllMuscles() {
+        this._muscleService
+            .getAllMuscleGroups()
+            .pipe(takeUntilDestroyed(this._destroyRef))
+            .subscribe({
+                next: (res) => {
+                    this.workout_muscles.set(res.musclesGroup);
+                    const allMuscles = [
+                        {
+                            _id: "1234",
+                            name: "full body",
+                            isActive: this._muscleService.activeMuscleGroup() === "1234",
+                        },
+                        ...res.musclesGroup.map((item) => ({
+                            ...item,
+                            isActive: item._id === this._muscleService.activeMuscleGroup(),
+                        })),
+                    ];
+                    this.workout_muscles.set(allMuscles);
+                },
+            });
     }
 }
