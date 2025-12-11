c549f01c42bd6ec508c8bf6d11033bc213e128fe
Reham-Gomaa <rehamgomaa686@gmail.com>
2025-12-11 09:19:43 +0200

    perf: ⚡️ use ngSrc

--- apps/Fitness/src/app/shared/components/ui/details/components/media-container/media-container.ts
+++ apps/Fitness/src/app/shared/components/ui/details/components/media-container/media-container.ts
@@ -1,11 +1,16 @@
-import {Component, input} from "@angular/core";
+import {Component, input, InputSignal} from "@angular/core";
+import {Exercise} from "../../../../../models/exercises";
+import {Meal} from "../../../../../models/meals";
+import {NgOptimizedImage} from "@angular/common";
 
 @Component({
     selector: "app-media-container",
-    imports: [],
+    imports: [NgOptimizedImage],
     templateUrl: "./media-container.html",
     styleUrl: "./media-container.scss",
 })
 export class MediaContainer {
-  type = input.required<'meal'|'class'>()
+    type = input.required<"meal" | "class">();
+    selectedExercise: InputSignal<Exercise | undefined> = input<Exercise | undefined>(undefined);
+    selectedMeal: InputSignal<Meal | undefined> = input<Meal | undefined>(undefined);
 }
