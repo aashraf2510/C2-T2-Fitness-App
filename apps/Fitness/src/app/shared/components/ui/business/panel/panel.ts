60e994907459f37858ba00005997b1db863902d0
Reham-Gomaa <rehamgomaa686@gmail.com>
2025-12-14 10:46:12 +0200

    feat: 🎸 (panel)optimize code,share selected meal&exercise

--- apps/Fitness/src/app/shared/components/ui/business/panel/panel.ts
+++ apps/Fitness/src/app/shared/components/ui/business/panel/panel.ts
@@ -1,5 +1,6 @@
 import {
     Component,
+    computed,
     DestroyRef,
     effect,
     inject,
@@ -18,14 +19,28 @@ import {Exercises} from "../../../../services/exercises/exercises";
 import {Levels} from "../../../../services/levels/levels";
 import {MealService} from "../../../../services/meals/meals";
 // Shared_Component
-import {NavTabs} from "../../navTabs/navTabs";
-import {TranslatePipe} from "@ngx-translate/core";
-import {Muscles} from "../../../../services/muscle/muscles";
 import {NgOptimizedImage} from "@angular/common";
+import {TranslatePipe} from "@ngx-translate/core";
+import {Skeleton} from "primeng/skeleton";
+import {Muscles} from "../../../../services/muscle/muscles";
+import {NavTabs} from "../../navTabs/navTabs";
+
+interface PanelConfig {
+    heading: string;
+    section: string;
+    listLabel: string;
+    emptyLabel: string;
+
+    list: Exercise[] | Meal[] | undefined;
+
+    onItemSelect: (item: Exercise | Meal) => void;
+    getItemLabel: (item: Exercise | Meal) => string;
+    getSrOnlyView: (item: Exercise | Meal) => string;
+}
 
 @Component({
     selector: "app-panel",
-    imports: [NavTabs, TranslatePipe, NgOptimizedImage],
+    imports: [NavTabs, TranslatePipe, NgOptimizedImage, Skeleton],
     templateUrl: "./panel.html",
     styleUrl: "./panel.scss",
 })
@@ -36,6 +51,7 @@ export class Panel {
     private readonly _muscles = inject(Muscles);
     private readonly _destroyRef = inject(DestroyRef);
 
+    loading: WritableSignal<boolean> = signal(true);
     panelType: InputSignal<string> = input<string>("");
     panelID: InputSignal<string> = input<string>("");
 
@@ -60,6 +76,37 @@ export class Panel {
             this.loadMeals(this.meals_id());
         }
     });
+    panelConfig = computed<PanelConfig>(() => {
+        const type = this.panelType();
+
+        return {
+            heading: `${type}.sr-only.heading`,
+            section: `${type}.sr-only.section`,
+            listLabel: `${type}.sr-only.ul`,
+            emptyLabel: `${type}.empty`,
+            list: type === "classes" ? this.exercesList() : this.mealsList(),
+            onItemSelect: (item: Exercise | Meal) =>
+                type === "classes"
+                    ? this.selectExercise(item as Exercise)
+                    : this.selectMeal(item as Meal),
+            getItemLabel: (item: Exercise | Meal) =>
+                type === "classes"
+                    ? `${(item as Exercise).exercise}, ${(item as Exercise).primary_equipment}`
+                    : (item as Meal).strMeal,
+            getSrOnlyView: (item: Exercise | Meal) =>
+                `${type}.sr-only.view | translate` +
+                " " +
+                (type === "classes" ? (item as Exercise).exercise : (item as Meal).strMeal),
+        };
+    });
+
+    getNavItems() {
+        return this.panelType() === "classes" ? this.difficultyLevels() : this.mealsCategories();
+    }
+
+    getNavItemsResponse(nav: navItem) {
+        return this.panelType() === "classes" ? this.getExercises(nav) : this.getMeals(nav);
+    }
 
     loadLevels(muscleId: string) {
         this._levels
@@ -86,12 +133,15 @@ export class Panel {
                 next: (res) => {
                     this.exercesList.set(res.exercises);
                     this.updateActiveTab(this.difficultyLevels, level);
+                    this.selectExercise();
                 },
+                complete: () => this.loading.set(false),
             });
     }
 
-    selectExercise(exercise: Exercise) {
-        this._muscles.setSelectedExercise(exercise);
+    selectExercise(exercise?: Exercise) {
+        if (exercise) this._muscles.setSelectedExercise(exercise);
+        else this._muscles.setSelectedExercise(this.exercesList()![0]);
     }
 
     loadMeals(mealsId: string) {
@@ -119,20 +169,30 @@ export class Panel {
                 next: (res) => {
                     this.mealsList.set(res.meals);
                     this.updateActiveTab(this.mealsCategories, meals);
+                    this.selectMeal();
                 },
+                complete: () => this.loading.set(false),
             });
     }
 
     updateActiveTab(tabsList: WritableSignal<navItem[] | undefined>, navitem: navItem) {
-        tabsList.update((value) =>
-            value?.map((item) => ({
+        tabsList.update((value) => {
+            if (!value) return value;
+            return value.map((item) => ({
                 ...item,
                 isActive: item._id === navitem._id,
-            }))
-        );
+            }));
+        });
+
+        if (this.panelType() === "classes") {
+            this.difficultyLevels.update((value) => [...(value || [])]);
+        } else {
+            this.mealsCategories.update((value) => [...(value || [])]);
+        }
     }
 
-    selectMeal(meal: Meal) {
-        this._mealService.setSelectedMeal(meal);
+    selectMeal(meal?: Meal) {
+        if (meal) this._mealService.setSelectedMeal(meal);
+        else this._mealService.setSelectedMeal(this.mealsList()![0]);
     }
 }
